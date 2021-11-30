17ca774f65ca2f0e7e9ad749f502a845138790ce
blemaire <bastien.lemaire@gmail.com>
2021-11-30 21:17:18 +0100

    Adding dynamique twitch iframe

--- pages/cast.vue
+++ pages/cast.vue
@@ -1,9 +1,62 @@
 <template>
     <div>
-        <v-select :options="casters" label="name"></v-select>
+        <v-select v-model="selectedCaster" :options="casters" label="name"></v-select>
+
+        <div class="iframe">
+            <iframe
+                :src="twitchUrl(selectedCaster)"
+                frameborder="0" allowfullscreen="true" scrolling="no" height="500px" width="80%"/>
+        </div>
     </div>
 </template>
 <script>
+const casters = [
+    {
+        channel: 'themole67',
+        name: 'Themole',
+    },
+    {
+        channel: 'ciskhan_',
+        name: 'Ciskhan',
+    },
+    {
+        channel: '?',
+        name: 'Colem',
+    },
+    {
+        channel: 'commandeur_rod',
+        name: 'Comm..._rod',
+    },
+    {
+        channel: 'elhyar_',
+        name: 'Elhyar',
+    },
+    {
+        channel: '?',
+        name: 'Iceofempire',
+    },
+    {
+        channel: 'melon_l_esturgeon',
+        name: 'Melo',
+    },
+    {
+        channel: 'luldorelle',
+        name: 'SMOC Vince',
+    },
+    {
+        channel: 'sylenixfr',
+        name: 'Sylenix',
+    },
+    {
+        channel: 'thalounette',
+        name: 'Thalounette',
+    },
+    {
+        channel: 'tutusasylum',
+        name: 'Tutus',
+    },
+].sort((casterA, casterB) => casterA.name.localeCompare(casterB.name));
+
 export default {
     asyncData(ctx) {
         // eslint-disable-next-line
@@ -12,52 +65,11 @@ export default {
     },
     data() {
         return {
-            casters: [
-                {
-                    name: 'Themole',
-                    url: 'https://www.twitch.tv/themole67',
-                },
-                {
-                    name: 'Ciskhan',
-                    url: 'https://www.twitch.tv/ciskhan_',
-                },
-                {
-                    name: 'Colem',
-                    url: '?',
-                },
-                {
-                    name: 'Comm..._rod',
-                    url: 'https://www.twitch.tv/commandeur_rod',
-                },
-                {
-                    name: 'Elhyar',
-                    url: 'https://www.twitch.tv/elhyar_',
-                },
-                {
-                    name: 'Iceofempire',
-                    url: '?',
-                },
-                {
-                    name: 'Melo',
-                    url: 'https://www.twitch.tv/melon_l_esturgeon',
-                },
-                {
-                    name: 'SMOC Vince',
-                    url: 'https://www.twitch.tv/luldorelle',
-                },
-                {
-                    name: 'Sylenix',
-                    url: 'https://www.twitch.tv/sylenixfr',
-                },
-                {
-                    name: 'Thalounette',
-                    url: 'https://www.twitch.tv/thalounette',
-                },
-                {
-                    name: 'Tutus',
-                    url: 'https://www.twitch.tv/tutusasylum',
-                },
-            ].sort((casterA, casterB) => casterA.name.localeCompare(casterB.name)),
+            casters,
+            selectedCaster: casters[0],
+            twitchUrl(caster) {
+                return `https://player.twitch.tv/?channel=${caster.name}&parent=damscreb.github.io&muted=true`;
+            },
         };
     },
 };
